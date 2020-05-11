461d2e8c58d7744a71e874d4a5a2c2d87ed9ff2f
yibudengtian <yibudengtian@Outlook.com>
2020-05-11 09:33:58 +0800

    添加创建文件模块，即使没有扫出任何端口，也会创建一个指定主机的结果文件

--- 000006_端口扫描器/tcp-socket-portscanner.py
+++ 000006_端口扫描器/tcp-socket-portscanner.py
@@ -2,21 +2,25 @@
 
 import socket
 
+address = 'xxx.xxx.xxx.xxx';
+f = open(address + ".txt", "w");
+f.close();
+
 def connect_try(ip, port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
         s.settimeout(1.1);  #经测试，默认的超时时间大概二十一二秒，等待时间太长了，一般连接成功也就1秒以内，改为1.1秒基本可判定是否能成功连接
         s.connect( (ip, port) );
         print("[*] " + ip + " %d/tcp open" % port);
-        with open("results.txt", "a") as f:
+        with open(address + ".txt", "a") as f:
             f.write(ip + ":" + str(port) + "\n");
     except:
         print("[*] " + ip + " %d/tcp closed" % port);
 
 def main():
-    port = 80;
-    for i in range(0, 256):
-        ip = '192.168.1.' + str(i);
+    for i in range(1, 65536):
+        ip = address;
+        port = i;
         connect_try(ip, port);
 
 if __name__ == "__main__":
